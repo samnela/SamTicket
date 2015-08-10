df168918ad65bb3769ff6fa7d288f543d4d91595
Samuel NELA <samuel.nela@external.grdf.fr>
2015-08-10 13:53:37 +0200

    It's time to work

---
+++ Vagrantfile
@@ -0,0 +1,18 @@
+# -*- mode: ruby -*-
+#  vi: set ft=ruby :
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+Vagrant.configure(2) do |config|
+  config.vm.box = "chef/debian-8.0"
+#  config.vm.provision :shell, path : "bootstrap.sh"
+  
+  config.vm.define "ticket" do |ticket|
+      ticket.vm.network "private_network" , ip:"10.10.10.10"
+      ticket.vm.synced_folder ".", "/var/www/"
+      ticket.vm.provision :shell , path: "bootstrap.sh"
+  end
+ 
+end
---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install apache2
+sudo apt-get install -y -f --force-yes vim  wget curl htop telnet rsync lynx unzip curl
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password hihihi'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password hihihi'
+sudo apt-get install -y -f --force-yes mysql-server
+sudo apt-get install -y -f --force-yes apache2-mpm-prefork
+
+sudo apt-get install -y -f --force-yes php5
+sudo service apache2 restart
+curl --silent --location https://deb.nodesource.com/setup_0.12 | bash -
+sudo apt-get install --yes  nodejs
+sudo apt-get install --yes build-essential
