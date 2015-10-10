a7ca820abf96248cddc311e7bde8f254b6e222d4
Samuel NELA <samuel.nela@gmail.com>
2015-10-10 19:17:28 +0200

    [project]   Symfony2.7 framework

---
+++ src/TicketBundle/Controller/TicketController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TicketBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class TicketController extends Controller
+{
+  
+}
---
+++ src/TicketBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TicketBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TicketControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+}
