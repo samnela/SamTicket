9d1eac52ffa0f765fd18d55b8d9749fc3ebcfa6e
snela <samuel.nela@gmail.com>
2017-04-11 22:43:59 +0200

    add role in final class

---
+++ src/TicketBundle/Entity/UserRoles.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TicketBundle\Entity;
+
+final class UserRoles
+{
+    const ROLE_DEFAULT = 'ROLE_USER';
+    
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+
+    const ROLE_MANAGER = 'ROLE_MANAGER';
+
+    const ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
+
+    const ROLE_AGENT = 'ROLE_AGENT';
+}
